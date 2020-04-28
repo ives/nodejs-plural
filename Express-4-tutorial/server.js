428c79b22f94386ee2abd8cfdba2a06f8783abde
Ives <ives@Ivans-Mac-mini.home>
2020-04-28 22:14:41 +0100

    express lint

--- Express-4-tutorial/server.js
+++ Express-4-tutorial/server.js
@@ -1,69 +1,69 @@
-const express = require("express");
-const chalk = require("chalk"); // pretty colours for console log / debug
-const morgan = require("morgan"); // log out http req details
-const path = require("path"); // deal with cross platform slashes, missing slashes etc - already available
+const express = require('express');
+const chalk = require('chalk'); // pretty colours for console log / debug
+const morgan = require('morgan'); // log out http req details
+const path = require('path'); // deal with cross platform slashes, missing slashes etc - already available
 // so only visible in debug mode:
-const debug = require("debug")("server"); // detailed logout
+const debug = require('debug')('server'); // detailed logout
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
-const confDemo = [{ condig: "demo" }, { passTo: "router" }];
+const confDemo = [{ condig: 'demo' }, { passTo: 'router' }];
 
 const app = express();
-app.use(morgan("tiny")); // combined - lots, tiny - min
+app.use(morgan('tiny')); // combined - lots, tiny - min
 
 // body-parser : pull out POST and add it to our body:
-// Parse incoming request bodies in a middleware before your handlers, 
+// Parse incoming request bodies in a middleware before your handlers,
 // available under the req.body property.
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // EJS templates:
-app.set("views", "./views"); // set a val, can then app.get('views') - those are reserved
-app.set("view engine", "ejs"); // SET not use - will look for an NPM package ejs
+app.set('views', './views'); // set a val, can then app.get('views') - those are reserved
+app.set('view engine', 'ejs'); // SET not use - will look for an NPM package ejs
 
-app.get("/", (req, res) => {
-  res.render("index2", {
-    moretitle: "this is Awesome shit",
+app.get('/', (req, res) => {
+  res.render('index2', {
+    moretitle: 'this is Awesome shit',
     list: [{ item: 12 }, { item: 321 }],
   });
 });
 
 // passing OPTIONAL config: (confDemo)
-const myRouter = require("./routes/myRoutes")(confDemo);
-const adminRouter = require("./routes/adminRoutes")();
-const authRouter = require("./routes/authRoutes")();
+const myRouter = require('./routes/myRoutes')(confDemo);
+const adminRouter = require('./routes/adminRoutes')();
+const authRouter = require('./routes/authRoutes')();
 
-app.use("/books", myRouter);
-app.use("/admin", adminRouter);
-app.use("/auth", authRouter);
+app.use('/books', myRouter);
+app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 // Middleware structure
 app.use((req, res, next) => {
-  debug("> my middleware");
+  debug('> my middleware');
   next();
 });
 // set up public folder
 // URL is then http://localhost:3000/style.css - does not include 'public'
-app.use(express.static(path.join(__dirname, "/public/")));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 // link to node module css - can have multiple places to look in
 app.use(
-  "./css",
-  express.static(path.join(__dirname, "./node_modules/bootstrap"))
+  './css',
+  express.static(path.join(__dirname, './node_modules/bootstrap')),
 );
 app.use(
-  "./css",
-  express.static(path.join(__dirname, "./node_modules/material-ui/css"))
+  './css',
+  express.static(path.join(__dirname, './node_modules/material-ui/css')),
 );
 
 // server gets a GET request to '/'
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // res.send('Hellooo');
   // __dirname locayion of executable
-  res.sendFile(path.join(__dirname, "views", "/index.html"));
+  res.sendFile(path.join(__dirname, 'views', '/index.html'));
 });
 
 app.listen(port, () => {
